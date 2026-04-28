a56eab7d8a64cb2845c11941693ee208bdeac607
ball-chi <1813536151@qq.com>
2026-04-28 11:17:17 +0800

    Update I2C_SC130GS_12801024_4Lanes_Config.v

--- I2C_SC130GS_12801024_4Lanes_Config.v
+++ I2C_SC130GS_12801024_4Lanes_Config.v
@@ -1,18 +1,4 @@
-/*-------------------------------------------------------------------------
-This confidential and proprietary software may be only used as authorized
-by a licensing agreement from CrazyBingo.www.cnblogs.com/crazybingo
-(C) COPYRIGHT 2012 CrazyBingo. ALL RIGHTS RESERVED
-Filename            :       I2C_SC130GS_12801024_Config.v
-Author              :       CrazyBingo
-Date                :       2019-08-03
-Version             :       1.0
-Description         :       I2C Configure Data of AR0135.
-Modification History    :
-Date            By          Version         Change Description
-===========================================================================
-19/08/03        CrazyBingo  1.0             Original
-24/06/06        CrazyStaff  1.1             Low_Rate
---------------------------------------------------------------------------*/
+
 
 `timescale 1ns/1ns
 
